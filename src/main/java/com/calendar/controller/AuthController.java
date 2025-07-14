20085cadf6cd0243cc461fd73082a935800fe8c0
Scott <ching8989889@gmail.com>
2025-07-14 16:27:57 +0800

    Fake Login API

---
+++ src/main/java/com/calendar/controller/AuthController.java
@@ -0,0 +1,35 @@
+package com.calendar.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+@RestController
+public class AuthController {
+
+    private static final List<String> users = Arrays.asList("爸爸", "媽媽", "哥哥", "妹妹");
+    @PostMapping("/login")
+    public ResponseEntity<?> login(@RequestBody Map<String, String> payload) {
+		System.out.println("Login:" + payload);
+        String username = payload.get("username");
+
+        if(users.contains(username)){
+            Map<String, Object> result = new HashMap<>();
+            result.put("username", username);
+            result.put("role", "member");
+            return ResponseEntity.ok(result);
+        }
+        else{
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("login fail or user not exist");
+        }
+    }
+
+}
--- src/main/java/com/calendar/controller/CalendarController.java
+++ src/main/java/com/calendar/controller/CalendarController.java
@@ -1,4 +1,4 @@
-package com.calendar;
+package com.calendar.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,6 +8,7 @@ public class CalendarController {
 	
 	@GetMapping("/test")
 	public boolean test(){
+		System.out.println("Test");
 		return true;
 	}
 }
