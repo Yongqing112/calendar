cb5a2ba171ddef7dbd3529288bf40c61c0293bb5
蕭永清 <ching8989889@gmail.com>
2025-09-23 11:55:56 +0800

    Change time type from LocalDate to LocalDateTime

--- src/main/java/com/calendar/repository/EventRepository.java
+++ src/main/java/com/calendar/repository/EventRepository.java
@@ -1,12 +1,20 @@
 package com.calendar.repository;
 
-import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.calendar.domain.Event;
 
 public interface EventRepository extends JpaRepository<Event, Long>{
-    List<Event> findByUsernameAndDateBetween(String username, LocalDate start, LocalDate end);
+
+    @Query("SELECT e FROM Event e " +
+           "WHERE e.createdBy = :createdBy " +
+           "AND e.startTime BETWEEN :start AND :end")
+    List<Event> findEventsInRange(@Param("createdBy") String createdBy,
+                                  @Param("start") LocalDateTime start,
+                                  @Param("end") LocalDateTime end);
 }
