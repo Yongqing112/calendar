c6701f7ba79787be4c73af0f63b01cfd562610de
蕭永清 <ching8989889@gmail.com>
2025-09-23 11:50:50 +0800

    Get Event and Update Event Revised Get All Events

--- src/main/java/com/calendar/controller/EventController.java
+++ src/main/java/com/calendar/controller/EventController.java
@@ -19,39 +19,61 @@ import com.calendar.repository.EventRepository;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
 
 @RestController
 @RequestMapping("/events")
 public class EventController {
 
-    private EventRepository eventRepository;
+    private final EventRepository repository;
 
-    EventController(EventRepository eventRepository) {
-        this.eventRepository = eventRepository;
+    EventController(EventRepository repository) {
+        this.repository = repository;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Event>> getAllEvents() {
+        List<Event> events = repository.findAll();
+        return ResponseEntity.ok(events);
     }
 
     @PostMapping
     public ResponseEntity<?> create(@RequestBody Event event) {
-        if (event.getUsername() != null) {
-            Event newEvent = eventRepository.save(event);
+        if (event.getCreatedBy() != null) {
+            Event newEvent = repository.save(event);
             return ResponseEntity.ok(newEvent);
         }
         return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Does not login.");
 
     }
 
-    @GetMapping
-    public ResponseEntity<List<Event>> getEvents(
-            @RequestParam String username, @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate start,
-            @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate end) {
-        List<Event> events = eventRepository.findAll();
-        return ResponseEntity.ok(events);
+    @GetMapping("/{id}")
+    public ResponseEntity<Event> getEvent(@PathVariable Long id) {
+        Event event = repository.findById(id).orElseThrow(null);
+        
+        return ResponseEntity.ok(event);
+    }
+    
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Event> updateEvent(@PathVariable Long id, @RequestBody Event updateEvent) {
+        Event event = repository.findById(id).map(e -> {
+            e.setTitle(updateEvent.getTitle());
+            e.setDescription(updateEvent.getDescription());
+            e.setStartTime(updateEvent.getStartTime());
+            e.setEndTime(updateEvent.getEndTime());
+            return repository.save(e);
+        }).orElseThrow(null);
+        
+        return ResponseEntity.ok(event);
     }
 
     @DeleteMapping
     public ResponseEntity<Map<String, String>> deleteEvent(@RequestParam Long id) {
         try {
-            eventRepository.deleteById(id);
+            repository.deleteById(id);
             Map<String, String> response = new HashMap<>();
             response.put("message", "刪除成功");
             return ResponseEntity.ok(response);
